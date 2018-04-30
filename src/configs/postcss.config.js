5aa4389a81ef6057b733bbf501e851899a8cad9a
Yuta Hiroto <git@about-hiroppy.com>
2018-04-30 22:04:14 +0900

    refactor(postcss): modify postcss.config.js

--- src/configs/postcss.config.js
+++ src/configs/postcss.config.js
@@ -7,22 +7,16 @@
  *   - autoprefixer
  */
 
+const common = {
+  plugins: [require('postcss-import'), require('postcss-mixins'), require('postcss-cssnext')]
+};
+
 const devConfig = (webpack) => {
   return {
-    plugins: [
-      require('postcss-reporter'),
-      require('postcss-browser-reporter'),
-      require('postcss-import'),
-      require('postcss-mixins'),
-      require('postcss-cssnext')
-    ]
+    plugins: [...common.plugins, require('postcss-reporter'), require('postcss-browser-reporter')]
   };
 };
 
-const prodConfig = (webpack) => {
-  return {
-    plugins: [require('postcss-import'), require('postcss-mixins'), require('postcss-cssnext')]
-  };
-};
+const prodConfig = (webpack) => common;
 
 module.exports = process.env.NODE_ENV !== 'production' ? devConfig : prodConfig;
