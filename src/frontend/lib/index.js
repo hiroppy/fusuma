7d4b9ff30b3aa512af0e575203cb4e6a587f0d00
Yuta Hiroto <git@about-hiroppy.com>
2018-05-04 08:34:50 +0900

    fix(webpack): enable dynamic loading as well

--- src/frontend/lib/index.js
+++ src/frontend/lib/index.js
@@ -3,9 +3,21 @@ import createBody from './create-body';
 import fetchSlides from './fetch-slides';
 import './setup-default';
 
-if (process.env.THEME === 'js') require('../assets/style/themes/javascript.css');
+if (process.env.THEME === 'js') {
+  try {
+    require('../assets/style/themes/javascript.css');
+  } catch (e) {
+    console.error(e);
+  }
+}
 
-if (process.env.CSS_PATH && process.env.CSS_PATH.match(/\+*.css$/i)) require(process.env.CSS_PATH);
+if (process.env.CSS_PATH && process.env.CSS_PATH.match(/\+*.css$/i)) {
+  try {
+    require(process.env.CSS_PATH);
+  } catch(e) {
+    console.error(e);
+  }
+}
 
 const slidesInfo = fetchSlides(require.context(process.env.SLIDE_PATH));
 
