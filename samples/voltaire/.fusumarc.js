f401e7fc646259fc836a784fd46aae8d0f0266b3
Yuta Hiroto <git@about-hiroppy.com>
2018-05-07 09:23:44 +0900

    feat(fusuma): implement theme

---
+++ samples/voltaire/.fusumarc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  meta: {
+    url: 'https://slide.hiroppy.me',
+    name: 'test-test',
+    author: 'hiroppy',
+    description: 'test',
+    thumbnail: 'url',
+    siteName: 'siteName'
+  },
+  slide: {
+    theme: 'bespoke-theme-voltaire'
+  },
+  extends: {
+    js: 'index.js',
+    css: 'style.css'
+  }
+};
--- src/frontend/lib/index.js
+++ src/frontend/lib/index.js
@@ -1,24 +1,36 @@
-import nebula from 'bespoke-theme-nebula';
 import createBody from './create-body';
 import fetchSlides from './fetch-slides';
 import './setup-default';
 
-if (process.env.THEME === 'js') {
-  try {
-    require('../assets/style/themes/javascript.css');
-  } catch (e) {
-    console.error(e);
+let theme;
+
+try {
+  switch (process.env.THEME) {
+    case 'js':
+      theme = require('bespoke-theme-nebula');
+      require('../assets/style/themes/javascript.css');
+      break;
+    case 'nebula':
+    case 'bespoke-theme-nebula':
+      theme = require('bespoke-theme-nebula');
+      break;
+    default:
+      theme = require(`${process.env.BASE_PATH}/node_modules/${process.env.THEME}`);
   }
+} catch (e) {
+  console.error(e);
 }
 
 if (process.env.CSS_PATH && process.env.CSS_PATH.match(/\+*.css$/i)) {
   try {
     require(process.env.CSS_PATH);
-  } catch(e) {
+  } catch (e) {
     console.error(e);
   }
 }
 
 const slidesInfo = fetchSlides(require.context(process.env.SLIDE_PATH));
 
-createBody(slidesInfo.slides, nebula);
+if (theme === undefined) throw new Error('Could not find theme');
+
+createBody(slidesInfo.slides, theme);
