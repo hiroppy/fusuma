d02efa61bc38f89276702978bfe59dac58110100
Yuta Hiroto <git@about-hiroppy.com>
2018-05-17 09:57:43 +0900

    refactor(bin): to separate logic from entry point

--- bin/fusuma.js
+++ bin/fusuma.js
@@ -1,109 +1,44 @@
 #!/usr/bin/env node
 
-const { join } = require('path');
-const ora = require('ora');
-const rmfr = require('rmfr');
 const prog = require('caporal');
-const remoteOriginUrl = require('remote-origin-url');
-const { init, read } = require('../src/configs/fusumarc');
-const { start, build, deploy, pdf } = require('../src');
+const taskProcess = require('./task-process');
 const { version } = require('../package.json');
-
-function getRemoteOriginUrl(basePath) {
-  return new Promise((resolve) => {
-    remoteOriginUrl(`${basePath}/.git/config`, (err, url) => {
-      if (err) {
-        console.error('Could not find github url');
-        resolve('');
-      } else {
-        url = url || '';
-
-        if (url.includes('git@')) resolve(`https://github.com/${url.split(':')[1]}`);
-        else resolve(url);
-      }
-    });
-  });
-}
-
-async function buildProcess(directory) {
-  const spinner = ora('Building with webpack').start();
-  const basePath = join(process.cwd(), directory || '');
-  const config = await read(basePath);
-  const remoteOrigin = await getRemoteOriginUrl(basePath);
-
-  await rmfr(join(basePath, 'dist'));
-  await build({
-    ...config,
-    internal: {
-      basePath,
-      remoteOrigin
-    }
-  });
-  spinner.stop();
-}
 
 prog
   .version(version)
   .description('CLI for easily make slides with markdown')
+
+  .command('init', 'Create a configure file')
+  .action(async (args, options, logger) => {
+    await taskProcess('init');
+  })
+
   .command('start', 'Start with webpack-dev-server')
   .option('-d <directory>', 'Directory to load')
   .action(async (args, options, logger) => {
-    const spinner = ora('Starting with webpack-dev-server').start();
-    const basePath = join(process.cwd(), options.d || '');
-    const config = await read(basePath);
-    const remoteOrigin = await getRemoteOriginUrl(basePath);
-
-    start(
-      {
-        ...config,
-        internal: {
-          basePath,
-          remoteOrigin
-        }
-      },
-      () => {
-        spinner.stop();
-      }
-    );
+    await taskProcess('start', { dir: options.d });
   })
 
   .command('build', 'Build with webpack')
   .option('-d <directory>', 'Directory to load')
   .action(async (args, options, logger) => {
-    await buildProcess(options.d);
-  })
-
-  .command('init', 'Create a configure file')
-  .action(async (args, options, logger) => {
-    console.log('Create new configure file.');
-    await init(process.cwd());
+    await taskProcess('build', { dir: options.d });
   })
 
   .command('deploy', 'Deploy to GitHub pages')
+  .option('-d <directory>', 'Directory to load')
   .action(async (args, options, logger) => {
-    const spinner = ora('Publishing to gh-pages').start();
-    const basePath = join(process.cwd(), options.d || '');
-
-    await deploy(join(basePath, 'dist'));
-
-    spinner.stop();
+    await taskProcess('deploy', { dir: options.d });
   })
 
   .command('pdf', 'Export as PDF')
   .option('-i <input>', 'Specified Directory')
   .option('-o <output>', 'Specified Filename')
   .action(async (args, options, logger) => {
-    const port = 3455;
-    const input = join(process.cwd(), options.i || 'dist');
-    const output = join(process.cwd(), options.o || 'slide.pdf');
-
-    await buildProcess();
-
-    const spinner = ora('Exporting as PDF').start();
-
-    await pdf(input, output, port);
-
-    spinner.stop();
+    await taskProcess('pdf', {
+      input: options.i,
+      output: options.o
+    });
   });
 
 prog.parse(process.argv);
