4f9bc305e7d92813c914b12c94d76dd9a89d8c5b
Yuta Hiroto <git@about-hiroppy.com>
2018-05-20 07:31:35 +0900

    feat(fusumarc): use object

--- src/configs/fusumarc.js
+++ src/configs/fusumarc.js
@@ -1,36 +1,50 @@
 'use strict';
 
-const fs = require('fs');
+const { readFile, writeFile } = require('fs');
 const { join, extname } = require('path');
 const { promisify } = require('util');
 const yaml = require('js-yaml');
 const pSearch = require('preferred-search');
 
-const readFileAsync = promisify(fs.readFile);
-const writeFileAsync = promisify(fs.writeFile);
+const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
-const configFileYml = `meta:
-  url:
-  name:
-  author:
-  description:
-  thumbnail:
-  siteName:
-  sns: # twitter, hatena
-    - twitter
-slide:
-  sidebar: true
-  theme: nebula
-extends:
-  js:
-  css:
-`;
+const config = {
+  meta: {
+    url: null,
+    name: null,
+    author: null,
+    thumbnail: null,
+    description: null,
+    sns: ['twitter']
+  },
+  slide: {
+    sidebar: true,
+    theme: 'nebula'
+  },
+  extends: {
+    js: null,
+    css: null
+  }
+};
 
 const configFileNames = ['.fusumarc.yml', '.fusumarc.js'];
 
+function getConfigYaml() {
+  const configYaml = yaml.safeDump(config).replace(/null/g, '');
+
+  return configYaml
+    .split('\n')
+    .map((s) => {
+      if (s.substr(-1) === ' ') return s.slice(0, -1);
+      else return s;
+    })
+    .join('\n');
+}
+
 async function init(baseDir) {
   try {
-    await writeFileAsync(join(baseDir, '.fusumarc.yml'), configFileYml);
+    await writeFileAsync(join(baseDir, '.fusumarc.yml'), getConfigYaml());
   } catch (e) {
     throw e;
   }
