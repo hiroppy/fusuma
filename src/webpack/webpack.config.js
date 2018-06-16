9e3a6dcd439f15e427393b1865558328a9c2a46d
Yuta Hiroto <git@about-hiroppy.com>
2018-06-16 12:20:23 +0900

    feat(webpack): make use of string-replace-loader and add css alias
    __body -> .bespoke-parent
    __bg -> .bespoke-backdrop
    __section-title -> .bespoke-backdrop.section-title

--- src/webpack/webpack.config.js
+++ src/webpack/webpack.config.js
@@ -49,6 +49,28 @@ module.exports = ({ meta, slide, extends: fileExtends, internal }) => {
             {
               loader: 'postcss-loader',
               options: require('../configs/postcss.config')()()
+            },
+            {
+              loader: 'string-replace-loader',
+              options: {
+                multiple: [
+                  {
+                    search: '__body',
+                    replace: '.bespoke-parent',
+                    flags: 'g'
+                  },
+                  {
+                    search: '__bg',
+                    replace: '.bespoke-backdrop',
+                    flags: 'g'
+                  },
+                  {
+                    search: '__section-title',
+                    replace: '.bespoke-backdrop.section-title',
+                    flags: 'g'
+                  }
+                ]
+              }
             }
           ]
         },
