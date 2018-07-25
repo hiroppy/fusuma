535bdf630dc110f307ca4838e58ade558d5dbeb3
Yuta Hiroto <git@about-hiroppy.com>
2018-07-26 07:42:55 +0900

    Updates

---
+++ fx/precache-manifest.da1bc3ed6ee51746c7e26c08b6d7cd63.js
@@ -0,0 +1,48 @@
+self.__precacheManifest = [
+  {
+    "url": "main.5.c24bacbbc2aa6e69ba46.css"
+  },
+  {
+    "revision": "042a49eaac67755441dcaad3ede32e07",
+    "url": "index.html"
+  },
+  {
+    "url": "presenter.host.1.4798ff47a3f95c3b1615.bundle.js"
+  },
+  {
+    "url": "presenter.view.2.3a417fff0443404ffc97.bundle.js"
+  },
+  {
+    "url": "runtime.13021ee723f440abc50b.bundle.js"
+  },
+  {
+    "url": "vendor.4.e295dd1e14175cabc34e.css"
+  },
+  {
+    "url": "vendor.4.e295dd1e14175cabc34e.bundle.js"
+  },
+  {
+    "url": "presenter.host.1.4798ff47a3f95c3b1615.css"
+  },
+  {
+    "url": "main.5.c24bacbbc2aa6e69ba46.bundle.js"
+  },
+  {
+    "url": "fee66e712a8a08eef5805a46892932ad.woff"
+  },
+  {
+    "url": "b06871f281fee6b241d60582ae9369b9.ttf"
+  },
+  {
+    "url": "af7ae505a9eed503f8b8e6982036873e.woff2"
+  },
+  {
+    "url": "Sidebar.0.7c60829710e36a1592a3.bundle.js"
+  },
+  {
+    "url": "760bd83ee04dff470e0277f3eb7deebe.svg"
+  },
+  {
+    "url": "674f50d287a8c48dc19ba404d20fe713.eot"
+  }
+];
--- fx/service-worker.js
+++ fx/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.f3ea029501fb7ccc8d8af9febed0cf7e.js"
+  "precache-manifest.da1bc3ed6ee51746c7e26c08b6d7cd63.js"
 );
 
 /**
