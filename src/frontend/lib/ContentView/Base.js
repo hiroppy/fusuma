6a18990fede2e2b67c59627215768c0e3e66aa16
Yuta Hiroto <git@about-hiroppy.com>
2018-07-26 02:30:37 +0900

    feat(frontend): implement lazy-load, fixes #15

--- src/frontend/lib/ContentView/Base.js
+++ src/frontend/lib/ContentView/Base.js
@@ -1,22 +1,33 @@
 /* eslint-disable  react/prop-types */
 
 import React from 'react';
+import Prism from 'prismjs';
 
-const Base = ({ slides, currentIndex, className = undefined }) => (
-  <article className={className}>
-    {slides.map((slide, i) => (
-      <section
-        key={i /* fix */}
-        className={slide.meta.className}
-        data-bespoke-backdrop={slide.meta.background}
-        data-bespoke-fx-direction={slide.meta.fx.direction}
-        data-bespoke-fx-transition={slide.meta.fx.transition}
-        dangerouslySetInnerHTML={{
-          __html: slide.context
-        }}
-      />
-    ))}
-  </article>
-);
+class Base extends React.PureComponent {
+  componentDidUpdate() {
+    Prism.highlightAll();
+  }
+
+  render() {
+    const { slides, currentIndex, className = undefined } = this.props;
+
+    return (
+      <article className={className}>
+        {slides.map((slide, i) => (
+          <section
+            key={i /* fix */}
+            className={slide.meta.className}
+            data-bespoke-backdrop={slide.meta.background}
+            data-bespoke-fx-direction={slide.meta.fx.direction}
+            data-bespoke-fx-transition={slide.meta.fx.transition}
+            dangerouslySetInnerHTML={{
+              __html: currentIndex >= i - 5 && currentIndex <= i + 5 ? slide.context : '<div />'
+            }}
+          />
+        ))}
+      </article>
+    );
+  }
+}
 
 export default Base;
