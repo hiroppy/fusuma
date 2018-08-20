8cdac77bb389ed0cc111832a6a4e36491405b1a4
Yuta Hiroto <git@about-hiroppy.com>
2018-08-21 08:17:11 +0900

    chore(webpack): update webpack-serve and webpack-stylish

--- src/webpack/webpack-serve.js
+++ src/webpack/webpack-serve.js
@@ -3,16 +3,17 @@
 const serve = require('webpack-serve');
 
 async function run(config, cb) {
-  const server = await serve({
-    config: {
-      ...config,
-      serve: {
-        dev: { logLevel: 'silent', publicPath: '/' },
-        hot: { logLevel: 'silent' },
-        logLevel: 'silent'
-      }
+  const server = await serve(
+    {},
+    {
+      config: {
+        ...config
+      },
+      logLevel: 'silent',
+      dev: { logLevel: 'silent', publicPath: '/' },
+      hot: { logLevel: 'silent' }
     }
-  });
+  );
   let flag = false;
 
   server.on('build-finished', () => {
