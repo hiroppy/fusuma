e7d8dc92248d0c0498abcb94f1b984a539047430
Yuta Hiroto <git@about-hiroppy.com>
2018-09-06 00:49:43 +0900

    refactor(frontend): don't create root element

--- src/frontend/lib/create-body.js
+++ src/frontend/lib/create-body.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './AppContainer';
-import createRoot from './create-root';
 
 const createBody = (slides = [], theme) => {
-  ReactDOM.render(
-    <AppContainer
-      theme={theme}
-      slides={slides}
-    />, createRoot()
-  );
+  ReactDOM.render(<AppContainer theme={theme} slides={slides} />, document.getElementById('root'));
 };
 
 export default createBody;
