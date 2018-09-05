cb2b0a939316704daa5b379ba2d381e856da70a1
Yuta Hiroto <git@about-hiroppy.com>
2018-09-06 00:59:00 +0900

    refactor: delete unnessesary file

--- src/frontend/lib/insert-slide.js
+++
@@ -1,8 +0,0 @@
-const insertSlide = (slides, slide, position) => {
-  const tmpSlides = slides.slice();
-  tmpSlides.splice(position, 0, slide);
-
-  return tmpSlides;
-};
-
-export default insertSlide;
