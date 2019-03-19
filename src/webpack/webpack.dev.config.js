b292adb9064e2d9928d1c738c240d515556c8747
Yuta Hiroto <git@about-hiroppy.com>
2019-03-19 15:47:31 +0000

    fix: get rid of webpack-serve

--- src/webpack/webpack.dev.config.js
+++ src/webpack/webpack.dev.config.js
@@ -1,11 +1,21 @@
 'use strict';
 
+const webpack = require('webpack');
+
 function dev() {
   return {
     mode: 'development',
     devtool: 'inline-cheap-module-source-map',
     output: {
       filename: 'bundle.js'
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+    devServer: {
+      hot: true,
+      quiet: true,
+      port: 8080,
+      inline: true,
+      contentBase: '.'
     }
   };
 }
