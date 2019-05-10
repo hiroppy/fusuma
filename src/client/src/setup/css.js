385f157ece9dd2a88278bd912cb77293508b34e4
Yuta Hiroto <git@about-hiroppy.com>
2019-05-10 17:12:57 +0100

    feat: add slide.code options

--- src/client/src/setup/css.js
+++ src/client/src/setup/css.js
@@ -1,3 +1,2 @@
 import '../../assets/style/webSlides.css';
-import 'prismjs/themes/prism.css';
 import '../../assets/style/common.css';
--- src/configs/babelrc.js
+++ src/configs/babelrc.js
@@ -1,21 +1,34 @@
 'use strict';
 
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          browsers: 'last 1 version'
+function babelrc({ languages = [], plugins = [], theme = 'default' } = {}) {
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            browsers: 'last 1 version'
+          }
         }
-      }
+      ],
+      '@babel/preset-react'
     ],
-    '@babel/preset-react'
-  ],
-  plugins: [
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread'
-  ],
-  compact: true
-};
+    plugins: [
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-object-rest-spread',
+      [
+        'prismjs',
+        {
+          languages,
+          plugins,
+          theme,
+          css: !!languages.length
+        }
+      ]
+    ],
+    compact: true
+  };
+}
+
+module.exports = babelrc;
