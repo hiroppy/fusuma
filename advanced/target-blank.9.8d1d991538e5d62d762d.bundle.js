4cea680036af21245fc8b09c1229988321e4ab3e
Yuta Hiroto <git@about-hiroppy.com>
2019-06-29 12:16:13 +0100

    Updates

---
+++ advanced/target-blank.9.8d1d991538e5d62d762d.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{639:function(t,n,e){"use strict";function o(){document.addEventListener("click",function(t){if("A"===t.target.tagName){if(t.target.host===location.host||""===t.target.href)return;t.preventDefault(),window.open(t.target.href,"_blank")}})}e.r(n),e.d(n,"setTargetBlank",function(){return o})}}]);
