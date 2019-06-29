abee659de01c0149f85b23b3a5a543875a843542
Yuta Hiroto <git@about-hiroppy.com>
2019-06-29 11:58:19 +0100

    feat(client): support HMR of mermaid

--- packages/client/src/components/ContentView/Base.js
+++ packages/client/src/components/ContentView/Base.js
@@ -15,6 +15,8 @@ export class Base extends React.Component {
 
         // for presenter:view
         window.slide.el.addEventListener('ws:slide-change', (e) => {
+          this.reloadChart();
+
           if (this.props.onChangeSlideIndex) {
             this.props.onChangeSlideIndex(e.detail.currentSlide0);
           }
@@ -23,21 +25,37 @@ export class Base extends React.Component {
     }
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    if (process.env.CHART) {
+      const { Mermaid } = await import(/* webpackChunkName: 'Mermaid' */ '../../setup/Mermaid');
+      this.mermaid = new Mermaid();
+      this.mermaid.init();
+    }
+
     Prism.highlightAll();
   }
 
   componentDidUpdate() {
     Prism.highlightAll();
+    this.reloadChart();
   }
 
   shouldComponentUpdate(nextProps, nextState) {
+    if (process.env.NODE_ENV !== 'production') {
+      this.reloadChart();
+    }
     if (this.props.hash !== nextProps.hash || !window.slide) {
       return true;
     }
 
     return false;
   }
+
+  reloadChart = () => {
+    if (process.env.CHART && this.mermaid) {
+      this.mermaid.reload();
+    }
+  };
 
   render() {
     const {
