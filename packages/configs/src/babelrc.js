cff5cb176d2ea8e4e0f0c6a8df367dd4a5a7ab03
Yuta Hiroto <git@about-hiroppy.com>
2019-07-07 20:24:57 +0100

    refactor(configs): get rid of babel-polyfill

--- packages/configs/src/babelrc.js
+++ packages/configs/src/babelrc.js
@@ -8,12 +8,20 @@ function babelrc({ languages = [], plugins = [], theme = 'default' } = {}) {
         {
           targets: {
             browsers: 'last 1 version'
-          }
+          },
+          modules: false,
+          useBuiltIns: 'usage',
+          corejs: {
+            version: 3,
+            proposals: true
+          },
+          debug: true
         }
       ],
       '@babel/preset-react'
     ],
     plugins: [
+      '@babel/plugin-transform-runtime',
       '@babel/plugin-syntax-dynamic-import',
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-proposal-object-rest-spread',
