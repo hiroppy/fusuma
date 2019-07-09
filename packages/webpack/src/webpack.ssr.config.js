813ad6522cc7a4b084221b2a17f31184e51c4997
Yuta Hiroto <git@about-hiroppy.com>
2019-07-09 22:22:07 +0100

    refactor(client): create entryPoints dir

--- packages/webpack/src/webpack.ssr.config.js
+++ packages/webpack/src/webpack.ssr.config.js
@@ -5,7 +5,6 @@ const { join, resolve } = require('path');
 function ssr({ clientBasePath }) {
   return {
     mode: 'none',
-    // entry: [join(clientBasePath, '/src/ServerApp.js')],
     target: 'node',
     devtool: false,
     output: {
