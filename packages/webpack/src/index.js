7c0f8c6bf4e490fe4dcdafa035abae3183822ede
maxmellon <tsujiken_melon@r.recruit.co.jp>
2019-09-04 19:12:03 +0900

    feat(fusuma): add option of port to the 'start-task'.

--- packages/webpack/src/index.js
+++ packages/webpack/src/index.js
@@ -4,8 +4,9 @@ const combineConfig = require('./webpack.config');
 
 async function start(config) {
   const server = require('./server');
+  const port = config.port || 8080
 
-  return await server(combineConfig('development', config), { port: 8080 });
+  return await server(combineConfig('development', config), { port });
 }
 
 async function build(config, isConsoleOutput = true, cb) {
