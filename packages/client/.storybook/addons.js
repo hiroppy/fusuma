6d188b954db2e2efe44376320c7ab694b6e548c8
Yuta Hiroto <git@hiroppy.me>
2021-01-24 12:21:49 +0900

    chore(client): remove storybook and some deps

--- packages/client/.storybook/addons.js
+++
@@ -1,1 +0,0 @@
-require('@storybook/addon-viewport/register');
--- packages/client/.storybook/config.js
+++
@@ -1,9 +0,0 @@
-const { configure } = require('@storybook/react');
-
-const req = require.context('../src/components', true, /.stories.js$/);
-
-function loadStories() {
-  req.keys().forEach((filename) => req(filename));
-}
-
-configure(loadStories, module);
